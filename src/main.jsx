1acd14c5e8a826676be761e67455076eef4d444e
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2025-05-28 09:25:00 -0400

    Fix App import path for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx'
+import App from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
