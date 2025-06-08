6873d8d8de5879005be24bc23b091d0a7ea49bd7
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2025-06-08 00:30:16 -0400

    fixed linking between db and railway

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,3 @@
-// backend/db.js
 import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
@@ -6,7 +5,10 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL || process.env.DB_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 export default pool;
