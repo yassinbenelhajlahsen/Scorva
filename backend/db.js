f511be2fa00f3577901b32c6f29fd5b51ae9994a
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2025-06-09 10:51:40 -0400

    fixed incorrect db url

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL || process.env.DB_URL,
   ssl: process.env.NODE_ENV === "production"
     ? { rejectUnauthorized: false }
     : false,
