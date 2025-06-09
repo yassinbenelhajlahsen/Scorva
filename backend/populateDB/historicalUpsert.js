502536583f5b766c9487f7bbac729e38649bf176
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2025-06-09 11:35:15 -0400

    updated Pool Setup for cron

--- backend/populateDB/historicalUpsert.js
+++ backend/populateDB/historicalUpsert.js
@@ -8,7 +8,10 @@ import {
 dotenv.config({ path: path.resolve('../.env') });
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL || process.env.DB_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 
--- backend/populateDB/hourlyUpsert.js
+++ backend/populateDB/hourlyUpsert.js
@@ -6,7 +6,10 @@ import path from 'path';
 dotenv.config({ path: path.resolve('../.env') });
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL || process.env.DB_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 (async () => {
