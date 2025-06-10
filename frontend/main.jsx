af8f0a5da31f590ea7f220f841f8d174f7b1c896
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2025-06-10 13:19:45 -0400

    fixed import for speed insights

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 import App from './app.jsx'
 
