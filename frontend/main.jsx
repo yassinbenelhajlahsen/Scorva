b2e29dc8a74c035d278deb6ed84b6494f5143801
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2025-06-10 13:22:10 -0400

    added site analytics

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './app.jsx'
 
@@ -8,5 +9,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
         <SpeedInsights />
+        <Analytics />
   </StrictMode>
 )
