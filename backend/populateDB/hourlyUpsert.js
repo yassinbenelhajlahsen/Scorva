65575ff01196c475720ebefb6ab80b27d67f6bf0
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2025-06-09 22:22:28 -0400

    changed log statment for cron for clarity

--- backend/populateDB/hourlyUpsert.js
+++ backend/populateDB/hourlyUpsert.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import { Pool } from "pg";
 import { runTodayProcessing } from "./src/eventProcessor.js";
 import path from 'path';
+import { DateTime } from "luxon";
 
 dotenv.config({ path: path.resolve('../.env') });
 
@@ -12,13 +13,24 @@ const pool = new Pool({
     : false,
 });
 
+const nowEST = DateTime.now().setZone("America/New_York");
+
+function addOrdinal(day) {
+  const s = ["th", "st", "nd", "rd"];
+  const v = day % 100;
+  return day + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+
+// Build formatted timestamp string
+const formattedTime = `${nowEST.toFormat("MMMM")} ${addOrdinal(nowEST.day)}, ${nowEST.toFormat("yyyy")} @ ${nowEST.toFormat("h:mma").toLowerCase()}`;
+
 (async () => {
   try {
     // For each league, fetch and process today’s events
     await runTodayProcessing("nba", pool);
     //await runTodayProcessing("nfl", pool); commented out to avoid unwanted updates to database
     await runTodayProcessing("nhl", pool);
-    console.log(`[✔️ ${new Date().toISOString()}] Hourly upsert ran successfully.`);
+    console.log(`[ ${formattedTime} ✅ Hourly upsert ran successfully.`);
   } catch (err) {
     console.error("❌ [hourlyUpsert] fatal error:", err);
   } finally {
