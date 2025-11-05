969e21b1e519bd4424cf1465c5b3bf44917d4fba
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2025-11-05 11:27:47 -0500

    fix import case for App component in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from "@vercel/analytics/react"
 import './index.css'
-import App from './app.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
