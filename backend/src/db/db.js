157c1ebfc08e73deb90ddab276a8ec4b120f6767
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-04-02 14:33:04 -0400

    fix: ensure database connection pool settings are correctly configured

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -5,6 +5,9 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || process.env.DB_URL,
+  max: 30,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
