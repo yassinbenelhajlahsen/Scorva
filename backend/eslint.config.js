abc2ff08749bed4332ba4fac29d2ac00b0755694
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-04-04 23:38:16 -0400

    fix: update ESLint ignore patterns to include test directories

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 
 export default [
-  { ignores: ['node_modules', 'src/generated'] },
+  { ignores: ['node_modules', 'src/generated', '__tests__/**'] },
 
   {
     files: ['**/*.js'],
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import react from 'eslint-plugin-react'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'src/__tests__/**'] },
 
   {
     files: ['**/*.{js,jsx}'],
