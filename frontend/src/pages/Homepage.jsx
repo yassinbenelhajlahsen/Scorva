bfb80d694600e82ebe3fdbda7d8a1fe7356d9145
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-05-08 18:00:07 -0400

    feat(rating): mount TopPerformancesCard on Homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -13,6 +13,7 @@ import { useUserPrefs } from "../hooks/user/useUserPrefs.js";
 import ErrorState from "../components/ui/ErrorState.jsx";
 import Skeleton from "../components/ui/Skeleton.jsx";
 import NewsSection from "../components/news/NewsSection.jsx";
+import TopPerformancesCard from "../components/cards/TopPerformancesCard.jsx";
 import { PullToRefresh } from "../components/ui/PullToRefresh.jsx";
 
 export default function Homepage() {
@@ -88,6 +89,9 @@ export default function Homepage() {
 
       {/* News Headlines */}
       <NewsSection />
+
+      {/* Top Performances */}
+      <TopPerformancesCard league="nba" />
 
       {/* League tabs + Games */}
       {error ? (
